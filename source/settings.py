b7acb13588ccee4e2cd6f9f29f365de02504d9ae
Damon Jablons <damonjablons@gmail.com>
2011-11-01 22:49:25 -0400

    making it look spiffy

--- source/settings.py
+++ source/settings.py
@@ -70,6 +70,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.auth',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
+    'django.core.context_processors.static',
 )
 
 if USE_I18N:
