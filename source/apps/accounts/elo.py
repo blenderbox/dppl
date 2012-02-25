a8de7fa5a9d4b31ee3edd68d63a09f8336e9500b
Damon Jablons <damonjablons@gmail.com>
2012-02-24 19:18:53 -0500

    Successfully implemented the trueskill system

--- source/apps/accounts/elo.py
+++ source/apps/accounts/elo.py
@@ -1,6 +1,7 @@
-from __future__ import division
+from trueskill import TrueSkill
 
 
+''' ELO Rankings are lame, let's use TrueSkill!
 def calculate_elo(winner, loser, draw=False):
     """ This function will calculate the new ELO rankings of each player's
     score which is fed in. In the case that the match is a draw, set draw to
@@ -21,3 +22,28 @@ def calculate_elo(winner, loser, draw=False):
             'winner': int(winner + (K * (scor_win - prob_win))),
             'loser': int(loser + (K * (scor_los - prob_los))),
             }
+'''
+
+
+def rank(player1, player2):
+    """ This function will calculate the TrueSkill ranking of each player. It
+    accepts two player arguments. A player is a dictionary containing the
+    player's 'mu', and 'sigma'. Player1 is the winner, and Player2 is the
+    loser.
+    """
+    DRAW_PROBABILITY = 0  # It's impossible to draw 1 on 1
+    ts = TrueSkill(draw_probability=DRAW_PROBABILITY)
+    team1 = (ts.Rating(**player1),)
+    team2 = (ts.Rating(**player2),)
+    r1, r2 = tuple(x[0] for x in ts.transform_ratings(
+        rating_groups=(team1, team2)))
+    return ({
+        'mu': r1.mu,
+        'sigma': r1.sigma,
+        'exposure': r1.exposure,
+        },
+        {
+        'mu': r2.mu,
+        'sigma': r2.sigma,
+        'exposure': r2.exposure,
+        })
