9573328a826242ad48eed1f7f4d7df03a893940c
Damon Jablons <damonjablons@gmail.com>
2012-03-13 09:55:30 -0400

    Updated the heroku script...

--- deploy/heroku.sh
+++ deploy/heroku.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
+# Bash colors
+BOLD="$(tput bold)"
+RESET="$(tput sgr0)"
+RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
+GREEN="$(tput setaf 2)"
+
+# Binaries
 GIT=$(which git)
 PYTHON=$(which python)
 MANAGE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../source/manage.py"
 
-$PYTHON $MANAGE collectstatic --noinput
-$PYTHON $MANAGE compress
-$GIT push heroku master
+function handle_error {
+  echo -e "${1}\t\t\t${RED}${BOLD}[Failed]${RESET}"
+  exit 1
+}
+
+fail=0
+
+$PYTHON $MANAGE collectstatic --noinput || fail=1
+if [ $fail -eq 1 ]; then
+  handle_error "Collect Static"
+fi
+
+$PYTHON $MANAGE compress || fail=1
+if [ $fail -eq 1 ]; then
+  handle_error "Compressor"
+fi
+
+$GIT push heroku master || fail=1
+if [ $fail -eq 1 ]; then
+  handle_error "Heroku"
+fi
+
+# Everything worked!
+echo -e "\n${BOLD}${GREEN}Deployed to Heroku, remember to run migrations manually!${RESET}"
