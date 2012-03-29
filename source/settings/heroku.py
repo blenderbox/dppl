c3c5dc476170b15d53840439748eac715f53f315
Damon Jablons <damonjablons@gmail.com>
2012-03-28 20:03:24 -0400

    Reverted

--- source/settings/heroku.py
+++ source/settings/heroku.py
@@ -76,7 +76,7 @@ AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', '')
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', '')
 AWS_QUERYSTRING_AUTH = False
-AWS_IS_GZIPPED = True
+# AWS_IS_GZIPPED = True
 COMPRESS_CSS_FILTERS = [
         'compressor.filters.css_default.CssAbsoluteFilter',
         'compressor.filters.cssmin.CSSMinFilter',
